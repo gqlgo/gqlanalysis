13b12930c352da1ac33309b6ead74c0d12b2914b
bannzai <kingkong999yhirose@gmail.com>
2022-12-13 15:39:20 +0900

    Support introspection header flag

--- multichecker/checker.go
+++ multichecker/checker.go
@@ -2,6 +2,7 @@ package multichecker
 
 import (
 	"flag"
+	"net/http"
 	"os"
 
 	"github.com/gqlgo/gqlanalysis"
@@ -13,16 +14,20 @@ var (
 	flagQuery  string
 )
 
+var ih = make(introspectionHeader)
+
 func init() {
 	flag.StringVar(&flagSchema, "schema", "schema/**/*.graphql", "pattern of schema")
 	flag.StringVar(&flagQuery, "query", "query/**/*.graphql", "pattern of query")
+	flag.Var(ih, "introspectionHeader", "format key1=value1,key2=value2")
 }
 
 func Main(analyzers ...*gqlanalysis.Analyzer) {
 	flag.Parse()
 	checker := &checker.Checker{
-		Schema: flagSchema,
-		Query:  flagQuery,
+		Schema:              flagSchema,
+		Query:               flagQuery,
+		IntrospectionHeader: http.Header(ih),
 	}
 
 	os.Exit(checker.Run(analyzers...))
